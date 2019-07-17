9c159ddba6f1f5a634147e5b0ca406e016cfb23a
pavan-bommideni <bpkpavan0208@gmail.com>
2019-07-18 01:02:49 +0530

    Added Code

--- src/test/java/com/example/demo/ConnectDatabaseMysqlApplicationTests.java
+++ src/test/java/com/example/demo/ConnectDatabaseMysqlApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ConnectDatabaseMysqlApplicationTests {
 
-//	@Test
-//	public void contextLoads() {
-//	}
+	@Test
+	public void contextLoads() {
+	}
 
 }
