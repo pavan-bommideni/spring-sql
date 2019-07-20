c260dbcce2363e2096b029c7a146620ec18b7fcb
pavan-bommideni <bpkpavan0208@gmail.com>
2019-07-20 05:35:00 +0530

    Code changes

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,12 +3,14 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.UserModel;
 import com.example.demo.service.UserService;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class UserController {
 	
 	@Autowired
