f76afbe9de2daf9ad48246c6a54cc7565074c129
pavan-bommideni <bpkpavan0208@gmail.com>
2019-07-23 22:58:52 +0530

    Code changes

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 @Entity
 public class User {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "Id")
 	private int id;
 	@Column(name = "Name")
