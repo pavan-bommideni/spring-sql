553642f1143ff7fbbe13eca30f3eb57b9eeddd56
Pavan Kumar Bommideni <bpkpavan0208@gmail.com>
2019-08-08 23:55:20 +0530

    Load data

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.example.demo.entity.User;
 import com.example.demo.model.UserModel;
 import com.example.demo.respository.IUserRepository;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class UserService {
@@ -25,6 +28,18 @@ public class UserService {
 	
 	public List<User> getUsers(){
 		return userRepository.findAll();
+	}
+
+	@PostConstruct
+	public void loadData(){
+		List<User> users = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			User user = new User();
+			user.setEmailAddress("P"+i+"@p.com");
+			user.setName("P"+i);
+			users.add(user);
+		}
+		userRepository.saveAll(users);
 	}
 
 }
